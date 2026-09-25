9c6af3d80156c268304d0454eef20f67d52d9c5e
LMG XENON <82078620+LMGXENON@users.noreply.github.com>
2026-09-25 09:58:23 +0100

    Time: 1 ms (34.74%), Space: 19.4 MB (86.64%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -2,7 +2,7 @@ class Solution:
     def fizzBuzz(self, n: int) -> list[str]:
         s = []
         
-        for i in range(1, n + 1):
+        for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
                 s.append("FizzBuzz")
             elif i % 3 == 0:
